0513b8f68c3e3c7381be530bafd3f5697bfc8e8f
Kyup <79992458+kyupp@users.noreply.github.com>
2025-12-06 11:56:40 -0600

    Cambios +Archivos para el Log

---
+++ ProyectoIII/src/main/java/Log/Log.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Log;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author gabri
+ */
+public class Log {
+
+    private static final String FILE = "Log.txt";
+
+    public static void log(String linea) {
+        try (FileWriter writer = new FileWriter(FILE, true)) {
+            writer.write(linea + System.lineSeparator());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
